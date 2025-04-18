b6855410b6360f5da22b68e3c75c8dbe778aa791
Azim Hatami <azimhatami.dev@gmail.com>
2025-04-18 14:56:37 +0330

    Update data in sequelize

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const { sequelize } = require('./configs/db.config.js');
 // require('./examples/models/model_2');
 // require('./examples/models/model_3');
 // require('./examples/crud/c_1');
-require('./examples/crud/r_1');
+// require('./examples/crud/r_1');
+require('./examples/crud/u_1');
